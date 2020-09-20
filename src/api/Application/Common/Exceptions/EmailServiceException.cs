c05ab11048de07bd161951755447ebe8a04dcdcf
Vlad <asdasfb326@gmail.com>
2020-09-20 18:44:56 +0300

    add email service exception

---
+++ src/api/Application/Common/Exceptions/EmailServiceException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Application.Common.Exceptions
+{
+    public class EmailServiceException : Exception
+    {
+        public EmailServiceException() : base()
+        {
+        }
+
+        public EmailServiceException(string message) : base(message)
+        {
+        }
+
+        public EmailServiceException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
